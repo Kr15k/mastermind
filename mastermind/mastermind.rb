c64d4fab3df5b0f42dd47c7c5ee6b0563f23a431
Kr15k <144447496+Kr15k@users.noreply.github.com>
2023-12-15 13:57:57 +0100

    Fixed leading 0's bug
    Fixed earlier versions problem with leading 0's in integer by now using strings. Updated input checker accordingly

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -15,27 +15,32 @@ module Mastermind
     end
 
     def generate_sequence(range = 10)
-      rand_num = rand(range)
-      @correct_order.append(rand(range))
+      rand_num = rand(range).to_s
+      @correct_order.append(rand_num.to_s)
       3.times do
-        next unless @multiples == false
+        if @multiples == false
 
-        rand_num = rand(range) while @correct_order.include?(rand_num)
-        @correct_order.append(rand_num)
+          rand_num = rand(range) while @correct_order.include?(rand_num.to_s)
+          @correct_order.append(rand_num.to_s)
+        elsif @multiples == true
+          @correct_order.append[rand_num.to_s]
+        end
       end
     end
 
     def check_if_right
+      exact = 0
+      close = 0
       @row[@round].each_index do |i|
-        @exact += 1 if @row[@round][i] == @correct_order[i]
+        exact += 1 if @row[@round][i] == @correct_order[i]
         @row[@round].each_index do |a|
-          @close += 1 if @correct_order[i] == @row[@round][a] && @row[@round][i] != @correct_order[i]
+          close += 1 if @correct_order[i] == @row[@round][a] && @row[@round][i] != @correct_order[i]
         end
       end
-      new_arr = [@exact, @close]
+      new_arr = [exact, close]
       @exact_close[@round] = new_arr
-      @exact = 0
-      @close = 0
+      exact = 0
+      close = 0
     end
 
     def check_game_state
@@ -55,12 +60,12 @@ module Mastermind
         puts "\n"
       end
       @row.each_index do |i|
-        puts "#{@row[i].join(' | ')}   #{@exact_close[i].join(' ')}   try nr.#{i + 1}"
+        puts "#{@row[i].join(' | ')}   #{@exact_close[i].join(' ')}   try: #{i + 1}"
       end
     end
 
-    def is_input_allowed(value)
-      if value.to_i.to_s == value && value.length == 4
+    def input_allowed(value)
+      if value.scan(/\D/).empty? && value.length == 4
         @error = false
       else
         @error = true
@@ -71,8 +76,8 @@ module Mastermind
 
     def input
       input = gets
-      is_input_allowed(input.chomp)
-      new_arr = input.delete('^0-9').split('').map(&:to_i)
+      input_allowed(input.chomp)
+      new_arr = input.delete('^0-9').split('').map(&:to_s)
       return unless @error == false
 
       @row[@round] = new_arr
